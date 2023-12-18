1855bd53aa8b058b1494516e511b80a2f6f6ed09
kevin <kevinooi1997@gmail.com>
2023-12-18 14:22:29 +0800

    added react-query for home page

--- src/components/notes/index.jsx
+++ src/components/notes/index.jsx
@@ -1,43 +1,53 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import Link from "next/link";
+import { useQuery } from "@tanstack/react-query";
+
+async function getNotes() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/notes`);
+  return await res.json();
+}
 
 const Notes = () => {
   // TODO: react query
-  useEffect(() => {
-    async function getNotes() {
-      const res = await fetch(`/api/notes`);
-      const { notes } = await res.json();
-      setNotes(notes);
-    }
-    getNotes();
-  }, []);
-
-  const [notes, setNotes] = useState([]);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["stream-hydrate-notes"],
+    queryFn: () => getNotes(),
+  });
 
   return (
     <div className="pt-20 flex flex-wrap sm:justify-start justify-center gap-5">
-      {notes.map((note) => (
-        <div
-          className="relative"
-          key={note.id}
-        >
-          <Link href={`/notes/${note.id}`}>
-            <div className="bg-gradient-to-br from-green-800 via-light-green-600 to-green-300 hover:from-green-600 p-4 w-[250px] h-[250px] rounded-lg cursor-pointer">
-              <h2 className="text-xl font-semibold mb-2 line-clamp-2 flex justify-between">
-                {note.title}
-              </h2>
-              <p
-                className="text-lg line-clamp-5"
-                dangerouslySetInnerHTML={{
-                  __html: note.content.replace(/\n/g, "<br />"),
-                }}
-              />
-            </div>
-          </Link>
-        </div>
-      ))}
+      {/* TODO: add loader */}
+      {isLoading && (
+        <h1 className="w-full flex justify-center items-center">Loading...</h1>
+      )}
+      {/* TODO: add error ui */}
+      {error && (
+        <h1 className="w-full flex justify-center items-center">
+          Oops! An error has occured!
+        </h1>
+      )}
+      {data != null &&
+        data?.notes.map((note) => (
+          <div
+            className="relative"
+            key={note.id}
+          >
+            <Link href={`/notes/${note.id}`}>
+              <div className="bg-gradient-to-br from-green-800 via-light-green-600 to-green-300 hover:from-green-600 p-4 w-[250px] h-[250px] rounded-lg cursor-pointer">
+                <h2 className="text-xl font-semibold mb-2 line-clamp-2 flex justify-between">
+                  {note.title}
+                </h2>
+                <p
+                  className="text-lg line-clamp-5"
+                  dangerouslySetInnerHTML={{
+                    __html: note.content.replace(/\n/g, "<br />"),
+                  }}
+                />
+              </div>
+            </Link>
+          </div>
+        ))}
     </div>
   );
 };
